3fd48ebdac0bc03748040a5c867f61160575049e
Augusto Lázaro <augusto.lr06@gmail.com>
2023-01-10 11:52:35 -0300

    feat: add TaskCreator component

---
+++ src/components/TaskCreator/TaskCreator.tsx
@@ -0,0 +1,18 @@
+import { PlusCircle } from 'phosphor-react'
+
+import styles from './TaskCreator.module.css'
+
+export function TaskCreator(){
+
+    return(
+        <div className={styles.TaskCreator}>
+
+            <input type="text" placeholder='Adicione uma nova tarefa' />
+            <button>
+                Criar
+                <PlusCircle size={16}/>
+            </button>
+
+        </div>
+    )
+}
